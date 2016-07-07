368c4707f93903d069cb853467d9efa03e9ed10c
handioq <ter.alexanderandr@gmail.com>
2016-07-07 21:02:06 +0300

    fix

--- app/src/main/java/com/github/handioq/fanshop/MainActivity.java
+++ app/src/main/java/com/github/handioq/fanshop/MainActivity.java
@@ -1,4 +1,4 @@
-package com.github.handioq;
+package com.github.handioq.fanshop;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.github.handioq.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
