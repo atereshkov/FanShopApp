7701c9d3dc48c8abffea5fa0599ed7bade231444
handioq <ter.alexanderandr@gmail.com>
2016-07-24 13:28:35 +0300

    image slider library added and impl

--- app/src/main/java/com/github/handioq/fanshop/productinfo/ProductInfoFragment.java
+++ app/src/main/java/com/github/handioq/fanshop/productinfo/ProductInfoFragment.java
@@ -6,14 +6,29 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
 import com.github.handioq.R;
 import com.github.handioq.fanshop.base.BaseFragment;
 import com.github.handioq.fanshop.model.Product;
 
-public class ProductInfoFragment extends BaseFragment implements ProductInfoView {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import butterknife.BindView;
+
+public class ProductInfoFragment extends BaseFragment implements ProductInfoView, BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
 
     private final static String TAG = "ProductInfoFragment";
+
+    @BindView(R.id.slider)
+    SliderLayout mDemoSlider;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,6 +46,39 @@ public class ProductInfoFragment extends BaseFragment implements ProductInfoView
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        initSlider();
+    }
+
+    private void initSlider(){
+        /* HashMap<String,String> url_maps = new HashMap<String, String>();
+        url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
+        url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
+        url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
+        url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");*/
+
+        List<String> urls = new ArrayList<>();
+        urls.add("http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
+        urls.add("http://tvfiles.alphacoders.com/100/hdclearart-10.png");
+        urls.add("http://cdn3.nflximg.net/images/3093/2043093.jpg");
+
+        for(String url : urls){
+            TextSliderView textSliderView = new TextSliderView(getActivity());
+
+            textSliderView
+                    .description("No description :(")
+                    .image(url)
+                    .setScaleType(BaseSliderView.ScaleType.Fit)
+                    .setOnSliderClickListener(this);
+
+            mDemoSlider.addSlider(textSliderView);
+        }
+
+        mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Default);
+        mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        mDemoSlider.setCustomAnimation(new DescriptionAnimation());
+        mDemoSlider.setDuration(4000);
+        mDemoSlider.addOnPageChangeListener(this);
     }
 
     @Override
@@ -53,4 +101,27 @@ public class ProductInfoFragment extends BaseFragment implements ProductInfoView
     public void setProduct(Product product) {
 
     }
+
+    @Override
+    public void onStop() {
+        // To prevent a memory leak on rotation, make sure to call stopAutoCycle() on the slider before activity or fragment is destroyed
+        mDemoSlider.stopAutoCycle();
+        super.onStop();
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+        //Toast.makeText(this,slider.getBundle().get("extra") + "",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+    @Override
+    public void onPageSelected(int position) {
+        Log.d("Slider Demo", "Page Changed: " + position);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {}
 }
