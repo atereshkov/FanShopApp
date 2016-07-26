eded725533fe11802838dae6c49457e55291318b
handioq <ter.alexanderandr@gmail.com>
2016-07-26 23:16:36 +0300

    viewpager slider implemented

--- app/src/main/java/com/github/handioq/fanshop/productinfo/ProductInfoFragment.java
+++ app/src/main/java/com/github/handioq/fanshop/productinfo/ProductInfoFragment.java
@@ -1,36 +1,42 @@
 package com.github.handioq.fanshop.productinfo;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.github.handioq.R;
 import com.github.handioq.fanshop.application.FanShopApp;
 import com.github.handioq.fanshop.base.BaseFragment;
-import com.github.handioq.fanshop.database.IProductRepository;
-import com.github.handioq.fanshop.database.ProductRepository;
 import com.github.handioq.fanshop.model.Image;
 import com.github.handioq.fanshop.model.Product;
-import com.github.handioq.fanshop.model.dbo.ProductDBO;
+import com.github.handioq.fanshop.productinfo.slider.ImageSliderAdapter;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import butterknife.BindView;
 
-public class ProductInfoFragment extends BaseFragment implements ProductInfoView {
+public class ProductInfoFragment extends BaseFragment implements ProductInfoView, ViewPager.OnPageChangeListener {
 
     private final static String TAG = "ProductInfoFragment";
 
     private int selectedItemId;
     private ProductInfoPresenter productInfoPresenter;
+
+    @BindView(R.id.imagePager)
+    ViewPager imageSlider;
+
+    @BindView(R.id.viewPagerCountDots)
+    LinearLayout viewPagerCountDots;
+
+    private int dotsCount;
+    private ImageView[] dots;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -64,7 +70,45 @@ public class ProductInfoFragment extends BaseFragment implements ProductInfoView
 
     private void initSlider(List<Image> images) {
 
+        imageSlider.setAdapter(new ImageSliderAdapter(getActivity(), images));
+        imageSlider.addOnPageChangeListener(this);
 
+        dotsCount = images.size();
+        dots = new ImageView[dotsCount];
+
+        for (int i = 0; i < dotsCount; i++) {
+            dots[i] = new ImageView(getActivity());
+            dots[i].setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.nonselecteditem_dot, null));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+
+            params.setMargins(4, 0, 4, 0);
+
+            viewPagerCountDots.addView(dots[i], params);
+        }
+
+        dots[0].setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.selecteditem_dot, null));
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        for (int i = 0; i < dotsCount; i++) {
+            dots[i].setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.nonselecteditem_dot, null));
+        }
+
+        dots[position].setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.selecteditem_dot, null));
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
 
     }
 
---
+++ app/src/main/java/com/github/handioq/fanshop/productinfo/slider/ImageSliderAdapter.java
@@ -0,0 +1,69 @@
+package com.github.handioq.fanshop.productinfo.slider;
+
+import android.content.Context;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.github.handioq.R;
+import com.github.handioq.fanshop.model.Image;
+
+import java.util.List;
+
+public class ImageSliderAdapter extends PagerAdapter {
+
+    private List<Image> images;
+    private LayoutInflater inflater;
+    private Context context;
+
+    public ImageSliderAdapter(Context context, List<Image> images) {
+        this.context = context;
+        this.images = images;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public int getCount() {
+        return images.size();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup view, int position) {
+        View imageLayout = inflater.inflate(R.layout.slider_image, view, false);
+
+        ImageView imageView = (ImageView) imageLayout
+                .findViewById(R.id.image_display);
+
+        Glide.with(view.getContext())
+                .load(images.get(position).getImage())
+                .into(imageView);
+
+        view.addView(imageLayout, 0);
+
+        return imageLayout;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view.equals(object);
+    }
+
+    @Override
+    public void restoreState(Parcelable state, ClassLoader loader) {
+    }
+
+    @Override
+    public Parcelable saveState() {
+        return null;
+    }
+
+}
