7b3ec293e53f7eacd13f8f554e7b67cfb442c844
handioq <ter.alexanderandr@gmail.com>
2016-08-04 13:45:11 +0300

    small fix

--- app/src/main/java/com/github/handioq/fanshop/application/FanShopApp.java
+++ app/src/main/java/com/github/handioq/fanshop/application/FanShopApp.java
@@ -1,7 +1,6 @@
 package com.github.handioq.fanshop.application;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.github.handioq.BuildConfig;
 import com.github.handioq.fanshop.di.component.CatalogComponent;
@@ -91,47 +90,4 @@ public class FanShopApp extends Application {
     public ProductInfoComponent getProductInfoComponent() {
         return productInfoComponent;
     }
-
-    public void setLoginComponent(LoginComponent loginComponent) {
-        this.loginComponent = loginComponent;
-    }
-
-    /*private static FanShopApp instance;
-    private NetworkService networkService;
-    private Realm realm;
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        instance = this;
-
-        onAppCreated();
-        checkBuild();
-    }
-
-    public static FanShopApp getInstance() {
-        return instance;
-    }
-
-    private void onAppCreated() {
-        networkService = new NetworkService();
-
-        RealmConfiguration config = new RealmConfiguration.Builder(this).build();
-        Realm.setDefaultConfiguration(config);
-        realm = Realm.getDefaultInstance();
-    }
-
-    private void checkBuild() {
-        if (BuildConfig.DEBUG) {
-            Log.d("BUILD", BuildConfig.BUILD_TYPE);
-        }
-    }
-
-    public NetworkService getNetworkService() {
-        return networkService;
-    }
-
-    public Realm getRealm() {
-        return realm;
-    }*/
 }
